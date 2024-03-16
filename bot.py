d2a3990408612b7bcaa0a72481e46a204304eef9
NoBadVibes79 <krazerog@outlook.com>
2024-03-16 12:08:44 +0300

    first settings add

---
+++ bot.py
@@ -0,0 +1,42 @@
+import asyncio
+import logging
+import conf
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from datetime import datetime
+
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=conf.TOKEN)
+dp = Dispatcher()
+dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+
+@dp.message(Command("dice"))
+async def cmd_dice(message: types.Message):
+    await message.answer_dice(emoji="🎲")
+
+
+
+@dp.message(Command("add_to_list"))
+async def cmd_add_to_list(message: types.Message, mylist: list[int]):
+    mylist.append(7)
+    await message.answer("Добавлено число 7")
+
+
+@dp.message(Command("show_list"))
+async def cmd_show_list(message: types.Message, mylist: list[int]):
+    await message.answer(f"Ваш список: {mylist}")
+
+
+@dp.message(Command("info"))
+async def cmd_info(message: types.Message, started_at: str):
+    await message.answer(f"Бот запущен {started_at}")
+
+
+async def main():
+    await dp.start_polling(bot, mylist=[1, 2, 3])
+
+if __name__ == "__main__":
+    asyncio.run(main())
