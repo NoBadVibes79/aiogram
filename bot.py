c411160ffe6ff1ffb08453ef99c930b1622f36aa
NoBadVibes79 <krazerog@outlook.com>
2024-04-03 19:25:15 +0300

    inline callbacks numver ended

--- bot.py
+++ bot.py
@@ -6,7 +6,9 @@ from aiogram.utils.formatting import Text, Bold
 from aiogram.utils.media_group import MediaGroupBuilder
 from aiogram.utils.markdown import hide_link
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.exceptions import TelegramBadRequest
 from config_reader import config
+from contextlib import suppress
 
 from datetime import datetime
 from random import randint
@@ -364,9 +366,6 @@ async def cmd_inline_url(message: types.Message, bot: Bot):
             text="Главный пользователь",
             url=f"tg://user?id={user_id}")
         )
-        print('Success', chat_info.has_private_forwards)
-    else:
-        print('Failed', chat_info.has_private_forwards)
 
     await message.answer(
         'Выберите ссылку',
@@ -390,6 +389,58 @@ async def cmd_random(message: types.Message):
 @dp.callback_query(F.data == "random_value")
 async def send_random_value(callback: types.CallbackQuery):
     await callback.message.answer(str(randint(1, 10)))
+    #! Очень важная строка, убирает часики
+    await callback.answer(
+        text="Спасибо, что воспользовались ботом!",
+        show_alert=True
+    )
+    # можно оставить в конце просто await callback.answer()
+
+
+# пример с счетчиком
+user_data = {}
+def get_keyboard():
+    buttons = [
+        [
+            types.InlineKeyboardButton(text="-1", callback_data="num_decr"),
+            types.InlineKeyboardButton(text="+1", callback_data="num_incr")
+        ],
+        [types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish")]
+    ]
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard
+
+
+async def update_num_text(message: types.Message, new_value: int):
+    with suppress(TelegramBadRequest):
+        await message.edit_text(
+            f"Укажите число: {new_value}",
+            reply_markup=get_keyboard()
+        )
+
+
+@dp.message(Command("numbers"))
+async def cmd_numbers(message: types.Message):
+    user_data[message.from_user.id] = 0
+    await message.answer("Укажите число: 0", reply_markup=get_keyboard())
+
+
+@dp.callback_query(F.data.startswith("num_"))
+async def callbacks_num(callback: types.CallbackQuery):
+    user_value = user_data.get(callback.from_user.id, 0)
+    action = callback.data.split("_")[1]
+
+    if action == "incr":
+        user_data[callback.from_user.id] = user_value+1
+        await update_num_text(callback.message, user_value+1)
+    elif action == "decr":
+        user_data[callback.from_user.id] = user_value-1
+        await update_num_text(callback.message, user_value-1)
+    elif action == "finish":
+        await callback.message.edit_text(f"Итого: {user_value}")
+
+    await callback.answer()
+
 
 async def main():
     await dp.start_polling(bot, mylist=[1, 2, 3])
